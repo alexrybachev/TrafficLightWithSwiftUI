574126c363f9136813fe7afd3f67e6a3ca265f81
Alex Rybachev <rybachev.as@gmail.com>
2022-05-11 22:43:09 +0400

    Initial Commit

---
+++ TrafficLightWithSwiftUI/TrafficLightWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightWithSwiftUIApp.swift
+//  TrafficLightWithSwiftUI
+//
+//  Created by Aleksandr Rybachev on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
